d3561bfc019a37fbad0402637ae6d5145a6e8eb5
Paul O'Connor <jamespauloconnor@gmail.com>
2018-01-12 13:32:48 -0500

    Starter items added

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/* 
+Gulp will be used for 
+development and deployment tasks, 
+such as minification of code, 
+translating SCSS into SASS & CSS,
+and reloading after a save.
+
+See the README.md for gulp commands.
+
+*/
+
+
---
+++ scss/README.md
@@ -0,0 +1,1 @@
+"# We'll use this file for discussing SCSS." 
