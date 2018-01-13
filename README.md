990e848edec72461b432970b47322a8819218008
Paul O'Connor <jamespauloconnor@gmail.com>
2018-01-12 20:19:01 -0500

    Sass and gulp watch hooked up

--- README.md
+++ README.md
@@ -8,15 +8,15 @@ This repo is a starter repo for helping students get working on building a coder
  * [module-2-gulp](https://github.com/ElevenfiftyAcademy/JavaScript-151-PortfolioStarter/tree/module-2-gulpsetup) - Set up Gulp for easy reload of html.
  * [module-3-bootstrap-and-jquery](https://github.com/ElevenfiftyAcademy/JavaScript-151-PortfolioStarter/tree/module-3-bootstrap-and-jquery) - Setting up basic jQuery and bootstrap. Testing with a nav bar.
  * [module-4-htmlstructure](https://github.com/ElevenfiftyAcademy/JavaScript-151-PortfolioStarter/tree/module-4-htmlstructure) - Setting up html structure for overall portfolio.
+ * [module-5-gulp-sass-setup](https://github.com/ElevenfiftyAcademy/JavaScript-151-PortfolioStarter/tree/module-5-gulp-sass-setup) - Setting up gulp for reloading SASS when changes happen.
 
 ## Current Branch Activity
-1. The sole focus of this branch was to add structure to the HTML page. No other files were affected.
+1. The sole focus of this branch is to set up Gulp so that it reloads changes to the .SCSS files.
+2. We will make a few small changes and additions to demonstrate reloading.
 
 ## Current Gulp Tasks
 - `gulp serve` uses browserSync & opens a project in the browser. Reloads when you save.
 
-## Inspiration
-Here is some reading for starting to get some style ideas.
-* [PortfolioList](https://github.com/iRaul/awesome-portfolios) - A list of amazing portfolios.
-* [Jake Albaugh](http://jakealbaugh.com/) - Jake Albaugh
-* [Free Code Camp List](https://medium.freecodecamp.org/15-web-developer-portfolios-to-inspire-you-137fb1743cae) - FreeCodeCamp top 15 list.
+## Reading
+Extra reading for setting up Gulp for Sass changes and compilation.
+* [Getting Started](http://ryanchristiani.com/getting-started-with-gulp-and-sass/) - A list of directions for setting up SASS changes.
--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,11 @@
 such as minification of code, translating SCSS into SASS & CSS,
 and reloading after a save.See the README.md for gulp commands.
 */
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var sass = require('gulp-sass');
 
-// Our objective is to make some html stubs and be able to have the browser change everytime we save.
-// So the first parameter is the gulp task name. This is what we call on the command line.
-// When we run gulp serve, it will fire a function that initializes browserSync and serves up
-// the index.html from the base of the project.
-// the gulp.watch will look for changes in the .html and the .css files.
-// At this point, try running gulp serve.
-// Then, with the app up, change both the index.html & the main.css files. Be sure to save.
-// It doesn't work if you just change the .css and save.
-// We'll add more hear later. 
+var reload = browserSync.reload;
 
 gulp.task('serve', function () {
     // Serve files from the root of the project
@@ -24,4 +16,28 @@ gulp.task('serve', function () {
         }
     });
     gulp.watch("*.html").on("change", reload);
+});
+
+// gulp.task('sass', function () {
+//     return gulp.src('scss/main.scss')
+//         .pipe(sass()) // Converts Sass to CSS with gulp-sass
+//         .pipe(gulp.dest('css'))
+// });
+
+// Compiles SCSS files from /scss into /css
+gulp.task('sass', function() {
+    return gulp.src('scss/main.scss')
+      .pipe(sass())
+     
+      .pipe(gulp.dest('css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
+  });
+
+
+// Runs sass & serve methods. Watches for changes and reloads. 
+gulp.task('dev', ['serve', 'sass'], function () {
+    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('*.html');
 });
