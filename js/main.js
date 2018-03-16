f1d5c7006d171053378be010615c638686e2fea0
Tracy Desserich <tdesserich@gmail.com>
2018-03-15 22:17:50 -0400

    portfolio updates

--- js/main.js
+++ js/main.js
@@ -13,24 +13,96 @@ jQuery(document).ready(function ($) {
     Code from a starter example by the creator. 
     https://codepen.io/dimsemenov/pen/hutrb
     -------------------*/
-    $('.with-caption').magnificPopup({
-        type: 'image',
-        closeBtnInside: false,
-        mainClass: 'mfp-with-zoom mfp-img-mobile',
+//     $('.with-caption').magnificPopup({
+//         type: 'image',
+//         closeBtnInside: false,
+//         mainClass: 'mfp-with-zoom mfp-img-mobile',
 
-        image: {
-            verticalFit: true,
-            titleSrc: function (item) {
-                //More could be done here......
-                //Also doesn't fit so good on iPad.
-                var caption = item.el.attr('title');
-                return caption;
+//         image: {
+//             verticalFit: true,
+//             titleSrc: function (item) {
+//                 //More could be done here......
+//                 //Also doesn't fit so good on iPad.
+//                 var caption = item.el.attr('title');
+//                 return caption;
+//             }
+//         },
+
+//         //This will let us click through images eventually....
+//         gallery: {
+//             enabled: true
+//         },
+//     });
+// });
+
+
+var myNavBar = {
+
+    flagAdd: true,
+
+    elements: [],
+
+    init: function (elements) {
+        this.elements = elements;
+    },
+
+    add : function() {
+        if(this.flagAdd) {
+            for(var i=0; i < this.elements.length; i++) {
+                document.getElementById(this.elements[i]).className += " fixed-theme";
             }
-        },
+            this.flagAdd = false;
+        }
+    },
 
-        //This will let us click through images eventually....
-        gallery: {
-            enabled: true
-        },
-    });
+    remove: function() {
+        for(var i=0; i < this.elements.length; i++) {
+            document.getElementById(this.elements[i]).className =
+                    document.getElementById(this.elements[i]).className.replace( /(?:^|\s)fixed-theme(?!\S)/g , '' );
+        }
+        this.flagAdd = true;
+    }
+
+};
+
+/**
+ * Init the object. Pass the object the array of elements
+ * that we want to change when the scroll goes down
+ */
+myNavBar.init(  [
+    "header",
+    "header-container",
+    "brand"
+]);
+
+/**
+ * Function that manage the direction
+ * of the scroll
+ */
+function offSetManager(){
+
+    var yOffset = 0;
+    var currYOffSet = window.pageYOffset;
+
+    if(yOffset < currYOffSet) {
+        myNavBar.add();
+    }
+    else if(currYOffSet == yOffset){
+        myNavBar.remove();
+    }
+
+}
+
+/**
+ * bind to the document scroll detection
+ */
+window.onscroll = function(e) {
+    offSetManager();
+}
+
+/**
+ * We have to do a first detectation of offset because the page
+ * could be load with scroll down set.
+ */
+offSetManager();
 });
